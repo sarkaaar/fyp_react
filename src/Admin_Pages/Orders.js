567e0b6adca19234406e3df2c973827067659e57
sarkaaar <waliabdul144@gmail.com>
2022-02-12 21:04:18 +0500

    pages

--- src/Admin_Pages/Orders.js
+++ src/Admin_Pages/Orders.js
@@ -2,7 +2,30 @@ import * as React from "react";
 import Header from "./admin_components/Header";
 
 export default function Orders() {
-
+  const data = [
+    {
+      username: "name",
+      products: [
+        { product: "product1" },
+        { product: "product2" },
+        { product: "product3" },
+      ],
+      price: [{ price: "12" }, { price: "23" }, { price: "34" }],
+      shippingAddress: "Lahore",
+      phone: "123",
+    },
+    {
+      username: "name",
+      products: [
+        { product: "product4" },
+        { product: "product5" },
+        { product: "product6" },
+      ],
+      price: [{ price: "12" }, { price: "23" }, { price: "34" }, { price: "54" }, { price: "564" }],
+      shippingAddress: "Lahore",
+      phone: "123",
+    },
+  ];
 
   return (
     <div>
