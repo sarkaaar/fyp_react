e53f44802e3f72246ef21f06c571ac6c085ad9fd
azmlhero <azmlhero1@gmail.com>
2022-02-22 00:58:03 +0000

    dashboard

--- src/Pages/Admin_Pages/admin_components/Header.js
+++ src/Pages/Admin_Pages/admin_components/Header.js
@@ -1,3 +1,4 @@
+import React from 'react';
 export default function Header() {
   return (
     <div>
@@ -51,9 +52,6 @@ export default function Header() {
           CATEGORIES
         </a>
 
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-</svg>
 
         <a
           href={"/admin/viewAllProducts"}
@@ -61,6 +59,12 @@ export default function Header() {
         >
           ALL PRODUCTS
         </a>
+
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+</svg>
+
+
          </div>
       <hr />
     </div>
