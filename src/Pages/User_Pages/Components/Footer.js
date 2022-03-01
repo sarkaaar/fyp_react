71ee84cdd1d062639680c279f72b253b027694a6
hassanarifbinarif <46750893+hassanarifbinarif@users.noreply.github.com>
2022-03-01 14:02:24 +0500

    Update Footer.js

--- src/Pages/User_Pages/Components/Footer.js
+++ src/Pages/User_Pages/Components/Footer.js
@@ -21,9 +21,15 @@ export default function Footer() {
         </div>
         <div>
           <h1 className="text-lg text-white p-2 font-bold">Careers</h1>
-          <h2 className="text-lg text-white p-2">Apply </h2>
-          <h2 className="text-lg text-white p-2">Become a Doctor</h2>
-          <h2 className="text-lg text-white p-2">PFAQ</h2>
+          <a href="/#">
+            <h2 className="text-lg text-white p-2">Apply </h2>
+          </a>
+          <a href="/#"> 
+            <h2 className="text-lg text-white p-2">Become a Doctor</h2>
+          </a>
+          <a href="/#">
+            <h2 className="text-lg text-white p-2">PFAQ</h2>
+          </a>
         </div>
         <div>
           <h1 className="text-lg text-white p-2 font-bold">Services</h1>
