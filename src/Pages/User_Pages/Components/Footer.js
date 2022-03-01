6cd7805289ca43784ab87c7582420912c3efc850
azmlhero <azmlhero1@gmail.com>
2022-03-01 02:02:21 +0000

    links on fotter order and favorites

--- src/Pages/User_Pages/Components/Footer.js
+++ src/Pages/User_Pages/Components/Footer.js
@@ -1,7 +1,7 @@
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
 export default function Footer() {
   return (
@@ -9,8 +9,12 @@ export default function Footer() {
       <div className="flex bg-gray-900 justify-around p-12">
         <div>
           <h1 className="text-lg text-white p-2 font-bold">My Account</h1>
-          <h2 className="text-lg text-white p-2">Orders</h2>
+          <a href="/orders">
+            <h2 className="text-lg text-white p-2">Orders</h2>
+          </a>
+          <a href="/favourites">
           <h2 className="text-lg text-white p-2">Favourites</h2>
+          </a>
           <h2 className="text-lg text-white p-2">Product Return</h2>
         </div>
         <div>
@@ -18,19 +22,28 @@ export default function Footer() {
           <h2 className="text-lg text-white p-2">Apply </h2>
           <h2 className="text-lg text-white p-2">Become a Doctor</h2>
           <h2 className="text-lg text-white p-2">PFAQ</h2>
-        </div><div>
+        </div>
+        <div>
           <h1 className="text-lg text-white p-2 font-bold">Services</h1>
           <h2 className="text-lg text-white p-2">Maps</h2>
           <h2 className="text-lg text-white p-2">Appointments</h2>
           <h2 className="text-lg text-white p-2">Online Check-Up</h2>
-        </div><div>
-          <h1 className="text-lg text-white p-2 font-bold">Social</h1>
-          <h2 className="text-lg text-white p-2"><FacebookIcon/> FaceBook</h2>
-          <h2 className="text-lg text-white p-2"><InstagramIcon/> Instagram</h2>
-          <h2 className="text-lg text-white p-2"><TwitterIcon/> Twitter</h2>
-          <h2 className="text-lg text-white p-2"><YouTubeIcon/> Youtube</h2>
         </div>
-        
+        <div>
+          <h1 className="text-lg text-white p-2 font-bold">Social</h1>
+          <h2 className="text-lg text-white p-2">
+            <FacebookIcon /> FaceBook
+          </h2>
+          <h2 className="text-lg text-white p-2">
+            <InstagramIcon /> Instagram
+          </h2>
+          <h2 className="text-lg text-white p-2">
+            <TwitterIcon /> Twitter
+          </h2>
+          <h2 className="text-lg text-white p-2">
+            <YouTubeIcon /> Youtube
+          </h2>
+        </div>
       </div>
 
       <div className=" flex justify-center bg-gray-800">
