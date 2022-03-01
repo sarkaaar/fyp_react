ee7175213387c0f423bde600f01de773de814a72
azmlhero <azmlhero1@gmail.com>
2022-03-01 01:37:43 +0000

    Order page and summary

--- src/Pages/User_Pages/Components/Orders.js
+++ src/Pages/User_Pages/Components/Orders.js
@@ -1,11 +1,82 @@
-import React from 'react'
-import Header from './Header'
+import React from "react";
+import Header from "./Header";
 export default function Orders() {
   return (
-
     <div>
-        <Header/>
-        aoa
-         </div>
-  )
+      <Header />
+
+      {/*  <> yay jab order na kia ho  </> */}
+      {/* yay dono jaga aye ga jahan order done b kia hu or na b kia hu Title  */}
+      <h1>Orders</h1>
+      <svg
+        class="w-6 h-6"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+      <p>No orders yet delevery free on 1st order </p>
+      <button>Start Shopping </button>
+
+      <> yay jab order kia ho </>
+      <div>
+        <h1>Order Confirmed</h1>
+        <p> we will inform you the delevery status of this order </p>
+        <svg
+          class="w-6 h-6"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+
+        <img src="abc" />
+        <h1>Order id :</h1>
+        <h1>Total</h1>
+      </div>
+
+      <div>
+        <h1>Order Details</h1>
+        <svg
+          class="w-6 h-6"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
+        </svg>
+        <svg
+          class="w-6 h-6"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
+        </svg>
+        <h1>Order ID :</h1>
+        <h1>Order Time :</h1>
+        <button>Cancel Order</button>
+
+        
+      </div>
+      <div>
+          <h1>Order Summery</h1>
+         <p>Quantity:</p>
+         <p>Price</p>
+         <p>Shipping </p>
+         <h1>Order Total</h1>
+          </div>
+      
+    </div>
+  );
 }
