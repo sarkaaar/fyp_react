6d58f58cf34f8a2fa433f3dd4dda87cd878cbee3
hassanarifbinarif <46750893+hassanarifbinarif@users.noreply.github.com>
2022-03-02 15:56:06 +0500

    Services.js updated

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -7,6 +7,8 @@ import { CardActionArea } from "@mui/material";
 import orders from "../../assets/images/orders.png";
 import Footer from "../User_Pages/Components/Footer";
 import doctor from "../../assets/images/doctor.png";
+import maps from "../../assets/images/maps.jpg";
+
 export default function Services() {
   return (
     <div>
@@ -55,19 +57,45 @@ export default function Services() {
 
       <div className="w-1/2  h-px bg-black m-auto"></div>
       <p className="text-4xl p-4 font-bold flex justify-center">
-        Services we provide
+        Services We Provide
       </p>
       <div className="w-1/2  h-px bg-black m-auto"></div>
 
       <div className="m-5 ml-36">
-        <a href="#" className="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-          <img className="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={doctor} alt="View Doctors" />
-          <div className="flex flex-col justify-between p-4 leading-normal">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">View Doctors</h5>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">View All World Qualified Veterinary Doctors Associated with us.</p>
+        <a href="viewDoctors" className="flex flex-col items-center bg-white rounded-lg border h-48 shadow-md md:flex-row hover:bg-gray-100 md:max-w-xl dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+          <div className="flex flex-row">
+            <img className="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={doctor} alt="View Doctors" />
+            <div className="flex flex-col justify-between p-4 leading-normal">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white ">View Doctors</h5>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">View All World Qualified Veterinary Doctors Associated With Us.</p>
+            </div>
           </div>
         </a>
       </div>
+
+      <div className="flex justify-end items-right w-full">
+        <div className="m-5 mr-36">
+          <a href="#" className="md:max-w-xl flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <div className="flex flex-col justify-between p-4 leading-normal min-w-[360px]" >
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white ">View Maps</h5>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">View All Clinics in the Vicinity.</p>
+            </div>
+            <img className="object-cover h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={maps} alt="View Maps" />
+          </a>
+        </div>
+      </div>
+
+      <div className="m-5 ml-36">
+        <a href="viewDoctors" className="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row hover:bg-gray-100 md:max-w-xl dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+          <img className="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={doctor} alt="View Doctors" />
+          <div className="flex flex-col justify-between p-4 leading-normal">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white ">Make Appointment</h5>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Make Appointmensts Online With Our Associated Doctors.</p>
+          </div>
+        </a>
+      </div>
+
+
 
       <div className="flex gap-4 mt-12">
         <a href="viewDoctors" target="_blank">
