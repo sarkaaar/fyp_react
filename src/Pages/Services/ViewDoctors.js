7ce34a039b9884694b0629ff09f0ef8c71d80f16
azmlhero <azmlhero1@gmail.com>
2022-03-14 15:22:05 +0000

    /viewDoctorsDB insert

--- src/Pages/Services/ViewDoctors.js
+++ src/Pages/Services/ViewDoctors.js
@@ -2,6 +2,8 @@ import { accordionSummaryClasses } from "@mui/material";
 import * as React from "react";
 import Header from "../User_Pages/Components/Header";
 import ammar from "../User_Pages/Components/images/ammar.jpg";
+
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 // Pages/User_Pages/Components/images
 // import * as React from "react";
 // import Header from "../User_Pages/Components/Header";
@@ -48,36 +50,40 @@ export default function ViewDoctors() {
         Click
       </Button>
       {/* <div className="  mx-20  my-20 flex flex-row rounded justify-between "> */}
-      <div className="max-w-3xl hover:bg-blue-200 border-2 border-black mx-auto flex p-6 bg-white rounded-lg shadow-xl  ">
-        <img src={ammar} className="m-5 w-33 h-40  " />
-
-        <div className=" text-xl text-gray-900 mx-auto p-6 space-between">
-          <div className=" text-3xl decoration-auto">
-            <h1>Name:Hafiz Ammar</h1>
-            <h1>Address: Comsats</h1>
-          </div>
-          <div className=" decoration-0">
-            <h1>Fee: 100</h1>
-
-            <h3>Rating: 5 </h3>
-          </div>
-          <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded">
-            Book Appointment{" "}
-          </button>
-        </div>
-      </div>
 
       <div>
         {doctors.map((item, key) => {
           return (
             <>
-              <h1>{item.name}</h1>
-              <h1>{item.clinicName}</h1>
-              <h1>{item.clinicPhone}</h1>
-              <h1>{item.ClinicAddress}</h1>
-              <h1>{item.description}</h1>
-              <h1>{item.fees}</h1>
-              <Button onClick={() => {navigate(`/makeAppointments/${item.id}`)}}>Book an Appointment</Button>
+              <div className="max-w-3xl hover:bg-blue-200 border-2 border-black mx-auto flex p-6 bg-white rounded-lg shadow-xl  ">
+                <AccountCircleIcon fontSize="large"/>
+
+                <div className=" text-xl text-gray-900 mx-auto p-6 space-between">
+                  <div className=" text-3xl decoration-auto">
+                    <h1>Name:{item.name}</h1>
+                    <h1>Clinic Name : {item.clinicName}</h1>
+                    <h1>Clinic Contact# :{item.clinicPhone}</h1>
+                    <h1>Clinical Address:{item.ClinicAddress}</h1>
+                  </div>
+                  <div className=" decoration-0">
+                    <h1>
+                      Fee:
+                      {item.fees}
+                    </h1>
+                    <h2>{item.description}</h2>
+                    <h3>Rating: 5 </h3>
+                  </div>
+                  {/* <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded"> */}
+                  <Button
+                    onClick={() => {
+                      navigate(`/makeAppointments/${item.id}`);
+                    }}
+                  >
+                    Book an Appointment
+                  </Button>
+                  {/* </bu/tton> */}
+                </div>
+              </div>
             </>
           );
         })}
