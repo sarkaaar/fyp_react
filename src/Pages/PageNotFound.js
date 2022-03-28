0f2dea94b23fced15c6a826603478974441dc331
azmlhero <azmlhero1@gmail.com>
2022-03-28 13:21:54 +0000

    not working 404 button

--- src/Pages/PageNotFound.js
+++ src/Pages/PageNotFound.js
@@ -10,10 +10,10 @@ export default function PageNotFound() {
       <h1>404</h1>
       <h2>Error Page Not Found</h2>
       <a
-        href="/"
-        className="inline flex px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
-      > 
-        Go to HomePage
+        href="viewDoctors"
+        className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
+      >
+        View Store
       </a>
     </div>
   );
