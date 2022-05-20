6d2234c4e1d71441b2d0a1b847653f7d29a162b4
azmlhero <azmlhero1@gmail.com>
2022-05-20 17:23:21 +0500

    staged

--- src/Pages/User_Pages/Profile/ProductReturns.js
+++ src/Pages/User_Pages/Profile/ProductReturns.js
@@ -57,11 +57,8 @@ export default function PRoductReturns() {
                     {item?.date?.toDate()?.toDateString()}
                   </td>
 
-<<<<<<< HEAD
                   {/* <td className=" text-lg p-2 px-8">{item?.user}</td> */}
-=======
->>>>>>> c6422cf533067776ac920a25d9408e7a4ce164b9
-                </tr>
+  </tr>
               </tbody>
             </>
           ))}
