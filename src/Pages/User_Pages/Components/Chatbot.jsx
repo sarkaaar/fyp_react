12074d0449013765a1198c09d292f6c752ce5083
sarkaaar <waliabdul144@gmail.com>
2022-05-21 21:29:25 +0500

    gashat

--- src/Pages/User_Pages/Components/Chatbot.jsx
+++ src/Pages/User_Pages/Components/Chatbot.jsx
@@ -1,5 +1,5 @@
-import ChatIcon from '@mui/icons-material/Chat';
-import { useState } from 'react';
+import ChatIcon from "@mui/icons-material/Chat";
+import { useState } from "react";
 
 export default function Chatbot() {
   const [question, setQuestion] = useState([]);
@@ -7,57 +7,57 @@ export default function Chatbot() {
 
   const trigger = [
     // 0
-    ['hi', 'hey', 'hello'],
+    ["hi", "hey", "hello"],
     // 1
-    ['how are you', 'how are things'],
+    ["how are you", "how are things"],
     // 2
-    ['what is going on', 'what is up'],
+    ["what is going on", "what is up"],
     // 3
-    ['happy', 'good', 'well', 'fantastic', 'cool'],
+    ["happy", "good", "well", "fantastic", "cool"],
     // 4
-    ['bad', 'bored', 'tired', 'sad'],
+    ["bad", "bored", "tired", "sad"],
     // 5
-    ['tell me story', 'tell me joke'],
+    ["tell me story", "tell me joke"],
     // 6
-    ['thanks', 'thank you'],
+    ["thanks", "thank you"],
     // 7
-    ['bye', 'good bye', 'goodbye'],
+    ["bye", "good bye", "goodbye"],
   ];
 
   const reply = [
     // 0
-    ['Hello!', 'Hi!', 'Hey!', 'Hi there!'],
+    ["Hello!", "Hi!", "Hey!", "Hi there!"],
     // 1
     [
-      'Fine... how are you?',
-      'Pretty well, how are you?',
-      'Fantastic, how are you?',
+      "Fine... how are you?",
+      "Pretty well, how are you?",
+      "Fantastic, how are you?",
     ],
     // 2
-    ['Nothing much', 'Exciting things!'],
+    ["Nothing much", "Exciting things!"],
     // 3
-    ['Glad to hear it'],
+    ["Glad to hear it"],
     // 4
-    ['Why?', 'Cheer up buddy'],
+    ["Why?", "Cheer up buddy"],
     // 5
-    ['What about?', 'Once upon a time...'],
+    ["What about?", "Once upon a time..."],
     // 6
-    ["You're welcome", 'No problem'],
+    ["You're welcome", "No problem"],
     // 7
-    ['Goodbye', 'See you later'],
+    ["Goodbye", "See you later"],
   ];
 
   const alternative = [
-    'Same',
-    'Go on...',
-    'Try again',
+    "Same",
+    "Go on...",
+    "Try again",
     "I'm listening...",
-    'Bro...',
+    "Bro...",
   ];
 
   function compare(triggerArray, replyArray, text) {
-    for (let x = 0; x < triggerArray.length; x++) {
-      for (let y = 0; y < replyArray.length; y++) {
+    for (let x = 0; x < triggerArray.length; x += 1) {
+      for (let y = 0; y < replyArray.length; y += 1) {
         if (triggerArray[x][y] === text) {
           setAnswer(
             replyArray[x][Math.floor(Math.random() * replyArray[x].length)],
@@ -69,32 +69,32 @@ export default function Chatbot() {
     return answer;
   }
 
-  function output(input) {
-    let text = input.toLowerCase().replace(/[^\w\s\d]/gi, '');
-    text = text
-      .replace(/ a /g, ' ')
-      .replace(/i feel /g, '')
-      .replace(/whats/g, 'what is')
-      .replace(/please /g, '')
-      .replace(/ please/g, '');
+  const robot = ["How do you do, fellow human", "I am not a bot"];
 
-    console.log('question = ', text);
+  function output(input) {
+    let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
+    text = text
+      .replace(/ a /g, " ")
+      .replace(/i feel /g, "")
+      .replace(/whats/g, "what is")
+      .replace(/please /g, "")
+      .replace(/ please/g, "");
+
+    console.log("question = ", text);
 
     if (compare(trigger, reply, text)) {
-      console.log('compare =', answer);
+      console.log("compare =", answer);
     } else if (text.match(/robot/gi)) {
       setAnswer(robot[Math.floor(Math.random() * robot.length)]);
-      console.log('robot =', answer);
+      console.log("robot =", answer);
     } else {
       setAnswer(alternative[Math.floor(Math.random() * alternative.length)]);
-      console.log('alternative = ', answer);
+      console.log("alternative = ", answer);
     }
   }
 
-  const robot = ['How do you do, fellow human', 'I am not a bot'];
-
   return (
-    <div className=" flex justify-end allign-end">
+    <div className=" flex justify-end allign-end bg-white">
       <div className="w-96 border rounded">
         <div>
           <div className="w-full">
@@ -121,7 +121,6 @@ export default function Chatbot() {
                   <div className="relative max-w-xl px-4 py-2 text-gray-700 rounded shadow">
                     <span className="block">
                       Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                      {' '}
                     </span>
                   </div>
                 </li>
@@ -138,7 +137,12 @@ export default function Chatbot() {
                 onChange={(e) => setQuestion(e.target.value)}
               />
 
-              <button type="submit" onClick={() => { output(question); }}>
+              <button
+                type="submit"
+                onClick={() => {
+                  output(question);
+                }}
+              >
                 <svg
                   className="w-5 h-5 text-gray-500 origin-center transform rotate-90"
                   xmlns="http://www.w3.org/2000/svg"
@@ -147,13 +151,11 @@ export default function Chatbot() {
                 >
                   <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
                 </svg>
-
               </button>
             </div>
           </div>
         </div>
       </div>
-
     </div>
   );
 }
