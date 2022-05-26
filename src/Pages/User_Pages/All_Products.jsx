c1578d34b728842e95d64aaf4a7c9709ee39112d
sarkaaar <waliabdul144@gmail.com>
2022-05-26 21:11:31 +0500

    conf

--- src/Pages/User_Pages/All_Products.jsx
+++ src/Pages/User_Pages/All_Products.jsx
@@ -90,11 +90,7 @@ export default function Products() {
                   style={{ width: '60px', height: '30px' }}
                 />
               </div>
-<<<<<<< HEAD
-               <div className="text-gray-700 block px-4 py-2 text-sm">
-=======
               <div className="text-gray-700 block px-4 py-2 text-sm">
->>>>>>> refs/remotes/origin/main
                 <label htmlFor="customRange2" className="form-label">
                   Select Range
                 </label>
@@ -111,11 +107,7 @@ export default function Products() {
 
                   <p>100</p>
                 </div>
-<<<<<<< HEAD
-              </div> 
-=======
               </div>
->>>>>>> refs/remotes/origin/main
             </div> */}
           </div>
 
