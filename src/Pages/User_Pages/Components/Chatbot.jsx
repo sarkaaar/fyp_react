b9e5c420cb2ccf8f9158187efed00d165007981c
sarkaaar <waliabdul144@gmail.com>
2022-05-30 08:24:18 +0500

    chat button

--- src/Pages/User_Pages/Components/Chatbot.jsx
+++ src/Pages/User_Pages/Components/Chatbot.jsx
@@ -1,6 +1,10 @@
 import ChatIcon from "@mui/icons-material/Chat";
 import { useState } from "react";
 
+import { Popover, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/solid";
+import { Fragment } from "react";
+
 export default function Chatbot() {
   const [question, setQuestion] = useState([]);
   const [answer, setAnswer] = useState([]);
@@ -96,65 +100,101 @@ export default function Chatbot() {
   }
 
   return (
-    <div className=" flex justify-end allign-end bg-white">
-      <div className="w-96 border rounded">
-        <div>
-          <div className="w-full">
-            <div className="relative flex items-center p-3 border-b border-gray-300">
-              <ChatIcon />
-              <span className="block ml-2 font-bold text-gray-600">
-                How May I Help You
-              </span>
-            </div>
-            <div className="relative w-full p-6 overflow-y-auto h-[25rem]">
-              <ul className="space-y-2">
-                {chat.map((msg, i) =>
-                  msg.from === "bot" ? (
-                    <li key={i} className="flex justify-start">
-                      <div className="relative max-w-xl px-4 py-2 text-gray-700 rounded shadow">
-                        <span className="block">{msg.text}</span>
-                      </div>
-                    </li>
-                  ) : (
-                    <li key={i} className="flex justify-end">
-                      <div className="relative max-w-xl px-4 py-2 text-gray-700 bg-gray-100 rounded shadow">
-                        <span className="block">{msg.text}</span>
-                      </div>
-                    </li>
-                  )
-                )}
-              </ul>
-            </div>
-
-            <div className="flex items-center justify-between w-full p-3 border-t border-gray-300">
-              <input
-                type="text"
-                placeholder="Message"
-                className="block w-full py-2 pl-4 mx-3 bg-gray-100 rounded-full outline-none focus:text-gray-700"
-                name="message"
-                required
-                onChange={(e) => setQuestion(e.target.value)}
+    <div className="  m-10">
+      <Popover className="relative">
+        {({ open }) => (
+          <>
+            <Popover.Button
+              className={`
+                ${open ? "" : "text-opacity-90"}
+                group inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-base font-medium text-white hover:text-opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
+            >
+              <span>Ask us Anything</span>
+              <ChevronDownIcon
+                className={`${open ? "" : "text-opacity-70"}
+                
+                  ml-2 h-5 w-5 text-orange-300 transition duration-150 ease-in-out group-hover:text-opacity-80`}
+                aria-hidden="true"
               />
+            </Popover.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute left-1/2 z-10 mt-3 w-fit max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl">
+                {/* // chat screen */}
+                <div className=" bg-white">
+                  <div className="w-96 border rounded">
+                    <div>
+                      <div className="w-full">
+                        <div className="relative flex items-center p-3 border-b border-gray-300">
+                          <ChatIcon />
+                          <span className="block ml-2 font-bold text-gray-600">
+                            How May I Help You
+                          </span>
+                        </div>
+                        <div className="relative w-full p-6 overflow-y-auto h-[25rem]">
+                          <ul className="space-y-2">
+                            {chat.map((msg, i) =>
+                              msg.from === "bot" ? (
+                                <li key={i} className="flex justify-start">
+                                  <div className="relative max-w-xl px-4 py-2 text-gray-700 rounded shadow">
+                                    <span className="block">{msg.text}</span>
+                                  </div>
+                                </li>
+                              ) : (
+                                <li key={i} className="flex justify-end">
+                                  <div className="relative max-w-xl px-4 py-2 text-gray-700 bg-gray-100 rounded shadow">
+                                    <span className="block">{msg.text}</span>
+                                  </div>
+                                </li>
+                              )
+                            )}
+                          </ul>
+                        </div>
 
-              <button
-                type="submit"
-                onClick={() => {
-                  output(question);
-                }}
-              >
-                <svg
-                  className="w-5 h-5 text-gray-500 origin-center transform rotate-90"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
-                </svg>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+                        <div className="flex items-center justify-between w-full p-3 border-t border-gray-300">
+                          <input
+                            type="text"
+                            placeholder="Message"
+                            className="block w-full py-2 pl-4 mx-3 bg-gray-100 rounded-full outline-none focus:text-gray-700"
+                            name="message"
+                            required
+                            onChange={(e) => setQuestion(e.target.value)}
+                          />
+
+                          <button
+                            type="submit"
+                            onClick={() => {
+                              output(question);
+                            }}
+                          >
+                            <svg
+                              className="w-5 h-5 text-gray-500 origin-center transform rotate-90"
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 20 20"
+                              fill="currentColor"
+                            >
+                              <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
+                            </svg>
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                {/* chatscreen ended */}
+              </Popover.Panel>
+            </Transition>
+          </>
+        )}
+      </Popover>
     </div>
   );
 }
