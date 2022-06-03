e4ea9d8286fec6d569f1f9eda4989c49f40d077b
hassanarifbinarif <46750893+hassanarifbinarif@users.noreply.github.com>
2022-06-03 17:23:25 +0500

    small changes

--- src/components/doctor/DoctorCard.jsx
+++ src/components/doctor/DoctorCard.jsx
@@ -42,7 +42,7 @@ export default function DoctorCard(person) {
               longitude: person?.obj.longitude,
               clinicName: person?.obj.clinicName,
             }}
-            className="inline-flex items-center rounded-lg bg-gradient-to-r from-indigo-600 to-blue-500 py-2 px-4 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="inline-flex transition duration-150 ease-in-out hover:scale-110 items-center rounded-lg shadow-indigo-600/50 shadow-lg bg-gradient-to-r from-indigo-600 to-blue-500 py-2 px-4 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:shadow-none"
           >
             View on Map
           </Link>
@@ -54,7 +54,7 @@ export default function DoctorCard(person) {
           </a> */}
           <a
             href={`/appointments/new/${person?.obj.id}`}
-            className="inline-flex items-center rounded-lg bg-gradient-to-r from-gray-400 to-gray-900 py-2 px-4 text-center text-sm font-medium text-white hover:opacity-90 focus:outline-none focus:ring-4 focus:ring-gray-200"
+            className="inline-flex transition duration-150 ease-in-out hover:scale-110 items-center rounded-lg shadow-gray-900/50 shadow-lg bg-gradient-to-r from-gray-400 to-gray-900 py-2 px-4 text-center text-sm font-medium text-white focus:outline-none focus:shadow-none"
           >
             Book Appointment
           </a>
