540dd41382c76f43700ffe94534063f4944aa17a
azmlhero <azmlhero1@gmail.com>
2022-06-20 20:02:19 +0500

    loader and modal at add productmodal

--- src/components/admin/inventory/AddEditProduct.jsx
+++ src/components/admin/inventory/AddEditProduct.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-array-index-key */
 import { useEffect, useState } from "react";
+import Modal from "@mui/material/Modal";
 import TextareaAutosize from "@material-ui/core/TextareaAutosize";
 import {
   Button,
@@ -19,18 +20,19 @@ import {
 // import Loader from "../../components/Loader/Loader";
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { db, storage } from "../../../firebase-config";
+import Loader from "../../../components/Loader/Loader";
 
 export default function AddEditProduct({ data }) {
   const [cat, setCat] = useState([]);
   const [loader, setLoader] = useState(false);
+  const [open, setOpen] = useState(false);
   const categoriesCollection = collection(db, "categories");
-
   const productsCollection = collection(db, "products");
+
   const [id, setID] = useState(data?.id);
   const [name, setName] = useState(data?.name);
   const [costPrice, setCostPrice] = useState(data?.costPrice);
   const [salePrice, setSalePrice] = useState(data?.salePrice);
-  // const z = data?.stock || 0;
   const [stock, setStock] = useState(data?.stock);
   const [category, setCategory] = useState(data?.category);
   const [description, setDescription] = useState(data?.description);
@@ -45,47 +47,13 @@ export default function AddEditProduct({ data }) {
       salePrice,
       stock,
       category,
-
       description,
       image: urls || "No Image Found",
     };
-    console.log(newProduct);
     await addDoc(productsCollection, newProduct);
   };
 
-  // const upload = () => {
-  //   console.log("upload images responded");
-  //   // console.log(image);
-  //   if (!image[0]) return;
-  //   const arr = [];
-
-  //   for (let i = 0; i < image.length; i += 1) {
-  //     console.log(image);
-
-  //   const storageRef = ref(storage, `products/${image[i].name}`);
-  //   const uploadTask = uploadBytesResumable(storageRef, image[i]);
-
-  //   uploadTask.on(
-  //     "state_changed",
-  //     (snapshot) => {
-  //       const prog = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-
-  //       setProgress(prog);
-  //     },
-  //     (error) => console.log(error),
-  //     () => {
-  //       getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-  //         console.log(url);
-  //         arr.push(url);
-  //         setUrls(arr);
-  //       });
-  //     }
-  //   );
-  //   }
-  // };
-
   const upload = () => {
-    
     if (!image[0]) return;
     const arr = [];
     for (let i = 0; i < image.length; i += 1) {
@@ -96,7 +64,7 @@ export default function AddEditProduct({ data }) {
         "state_changed",
         (snapshot) => {
           const prog = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          
+
           setProgress(prog);
           setLoader(false);
         },
@@ -190,41 +158,6 @@ export default function AddEditProduct({ data }) {
         onChange={(e) => setStock(e.target.value)}
         value={stock}
       />
-      {/* 
-      {variants?.map(([variant, quantity], i) => (
-        <div key={i} className="flex items-center gap-4">
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            type="text"
-            label="Varients"
-            onChange={(e) => updateVariant([e.target.value, quantity], i)}
-            value={variant}
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            type="text"
-            label="Qunatity"
-            onChange={(e) => updateVariant([variant, +e.target.value], i)}
-            value={quantity}
-          />
-          <IconButton type="button" onClick={() => removeVariant(i)}>
-            x
-          </IconButton>
-        </div>
-      ))}
-      <Button
-        sx={{ marginTop: "10px", marginBottom: "10px" }}
-        type="button"
-        fullWidth
-        variant="outlined"
-        onClick={() => setVariants([...variants, ["", 0]])}
-      >
-        Add variant
-      </Button> */}
 
       <div>
         <FormControl fullWidth style={{ margin: "10px 0" }}>
@@ -262,43 +195,59 @@ export default function AddEditProduct({ data }) {
         }}
         multiple
       />
-      <Button
+      <button
+        className=" flex h-12 w-full items-center justify-center rounded-md border bg-indigo-600 py-3 px-8 text-base font-medium text-white "
         type="submit"
-        fullWidth
-        variant="contained"
-        className="mt-6 mb-12"
         onClick={upload}
       >
         Images Upload
-      </Button>
-      <h3>
-        Uploaded
-        {progress}
-      </h3>
-      <hr className="mt-6" />
+      </button>
+      <button
+        className=" flex h-12 w-full items-center justify-center rounded-md border bg-indigo-600 py-3 px-8 text-base font-medium text-white "
+        disabled
+      >
+        <div className="flex gap-4">
+          <div className="flex">
+            <Loader />
+          </div>
+          {/* <h1 className="text-white text-center">Uploading...</h1> */}
+        </div>
+      </button>
+
       {data ? (
-        <Button
+        <button
+          className=" flex h-12 w-full items-center justify-center rounded-md border bg-indigo-600 py-3 px-8 text-base font-medium text-white "
           type="submit"
-          fullWidth
-          variant="contained"
-          className="mt-6 mb-12"
           onClick={() => {
             updateProduct(id);
           }}
         >
           Update Product
-        </Button>
+        </button>
       ) : (
-        <Button
+        <button
+          className=" mt-8 flex h-12 w-full items-center justify-center rounded-md border bg-indigo-600 py-3 px-8 text-base font-medium text-white "
           type="submit"
-          fullWidth
-          variant="contained"
-          className="mt-6 mb-16"
-          onClick={addProduct}
+          onClick={()=>{ addProduct(); 
+          setOpen(true);}}
         >
           Add Product
-        </Button>
+        </button>
       )}
+
+      <Modal
+        open={open}
+        onClose={() => {
+          setOpen(false);
+          window.location.reload(false);
+        }}
+      >
+        <div className="absolute top-1/2 left-1/2 w-[400px] -translate-y-1/2 -translate-x-1/2 rounded-lg bg-white p-4 shadow-lg">
+          <h1 className="p-4 text-center text-xl font-bold">
+            Password Change Successfully
+          </h1>
+        </div>
+      </Modal>
     </div>
   );
 }
