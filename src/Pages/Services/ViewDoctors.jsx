ff1f5b4a0604a91b5c69e48b73e5f56828a55992
azmlhero <azmlhero1@gmail.com>
2022-06-23 20:06:55 +0500

    Appointment booked only by logged in users

--- src/Pages/Services/ViewDoctors.jsx
+++ src/Pages/Services/ViewDoctors.jsx
@@ -1,26 +1,36 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
 import { collection, getDocs, query, where } from "firebase/firestore";
-import { db } from "../../firebase-config";
+import { db,auth } from "../../firebase-config";
 import Footer from "../User_Pages/Components/Footer";
 import UseMainLayout from "../../layouts/UserMainLayout";
 import DoctorCard from "../../components/doctor/DoctorCard";
-
+import Modal from "@mui/material/Modal";
+import { onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 export default function ViewDoctors() {
+  const [user, setUser] = useState({});
+  const [open, setOpen] = useState(false);
   const [doctors, setDoctor] = useState([]);
   const doctorsCollection = collection(db, "doctors");
-
+  const navigate = useNavigate();
   const getDoctor = async () => {
     const q = query(doctorsCollection, where("status", "==", false));
 
-    await getDocs(q).then((res) => {
-      setDoctor(res.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    });
+    if (user) {
+      await getDocs(q).then((res) => {
+        setDoctor(res.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      });
+    } else setOpen(true);
   };
 
   useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
     getDoctor();
-  }, []);
+  }, [user]);
 
   return (
     <UseMainLayout>
@@ -39,6 +49,19 @@ export default function ViewDoctors() {
         </div>
       </div>
       <Footer />
+      <Modal
+        open={open}
+        onClose={() => {
+          setOpen(false);
+          navigate("/sign_in");
+        }}
+      >
+        <div className="absolute top-1/2 left-1/2 w-[400px] -translate-y-1/2 -translate-x-1/2 rounded-lg bg-white p-4 shadow-lg">
+          <h1 className="p-4 text-center text-xl font-bold">
+            Please Login for this facility
+          </h1>
+        </div>
+      </Modal>
     </UseMainLayout>
   );
 }
